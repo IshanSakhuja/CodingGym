c3cd689c7f8c7f0b94b7b4bf8be9b39b6bafc78a
Ishan Sakhuja <42608456+IshanSakhuja@users.noreply.github.com>
2020-08-01 07:34:56 +0530

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,11 @@
+//https://www.hackerrank.com/challenges/flipping-bits/problem
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	unsigned int a,b,c,temp,size,num,i,j,ishan,count=0;
+	cin >> num;
+	num = ~num;
+	cout << num << endl;
+	return 0;
+}
